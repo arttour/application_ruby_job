f6b9a7183306b454b0995e55b42b6b25511a719e
arttour <dwatts@arttour.com>
2015-06-18 17:53:54 -0700

    Fixing spce

--- test/integration/default/serverspec/apps_spec.rb
+++ test/integration/default/serverspec/apps_spec.rb
@@ -6,13 +6,19 @@ puts "os: #{os}"
 
 if %w(debian ubuntu redhat suse fedora).include? os[:family]
 
-  describe file('/var/gnma_tli') do
+  describe file('/var/example') do
     it { should be_directory }
     it { should be_mode 755 }
-    it { should be_owned_by 'root' }
-    it { should be_grouped_into 'root' }
+    it { should be_owned_by 'www-data' }
+    it { should be_grouped_into 'www-data' }
   end
 
+  describe file('/var/example2') do
+    it { should be_directory }
+    it { should be_mode 755 }
+    it { should be_owned_by 'www-data' }
+    it { should be_grouped_into 'www-data' }
+  end
   
 
 end
